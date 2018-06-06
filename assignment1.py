96fbcb39167ec894cc7d00e831186aedbc5dab6e
lovishagumber <gumberlovisha7@gmail.com>
2018-06-07 00:12:59 +0530

    assignment1

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+#Q1 print anything
+print("i am lovisha")
+
+#Q2 join two strings
+print("acad"+"view")
+
+#Q3 print values of x,y,z
+x=2
+y=3
+z=4
+print(x,y,z)
+
+#Q4 print let's get started
+print("let\'s get started")
+
+#Q5
+s="acadview"
+course="python"
+fee=5000
+print("hi you are in %s, your course is %s and fee is %d"%(s,course,fee))
+
+#Q6
+name="tony stark"
+salary=1000000
+print("%s %d"%(name,salary))
