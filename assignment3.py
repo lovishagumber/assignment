a5d20a0dc981d4ccda5c4599c316bacfa12b63a9
lovishagumber <gumberlovisha7@gmail.com>
2018-06-07 17:33:43 +0530

    assignment

---
+++ assignment3.py
@@ -0,0 +1,35 @@
+#Q1
+l=[]
+a=input("enter no")
+l.append(a)
+print(l)
+
+#Q2
+l2=["google", "apple", "facebook", "microsoft", "tesla"]
+l.append(l2)
+print(l)
+print(l[1][2])
+
+#Q3
+l3=[1,2,1,4,6]
+a=l3.count(1)
+print(a)
+
+#Q4
+l4=[9,7,2,5,0,5]
+l4.sort()
+print(l4)
+
+#Q5
+c=[1,4,6]
+d=[3,1,7]
+e=c+d
+e.sort()
+print(e)
+
+#Q6
+l5=[1,4,7,2]
+z=l5.append(6)
+print(z)
+l.pop()
+
