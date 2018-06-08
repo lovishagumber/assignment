20212b96ad8a9b2f44736b2f849be2ab3bde718d
lovishagumber <gumberlovisha7@gmail.com>
2018-06-08 14:46:58 +0530

    assignment

---
+++ assignment4.py
@@ -0,0 +1,33 @@
+#Q1
+t=(1,2,3)
+a=len (t)
+print(a)
+
+#Q2
+t=(1,2,3,4)
+print(max(t))
+print(min(t))
+
+#Q3
+a=(1*2*4)
+print(a)
+
+#Q4
+a=set([1,2,4,8,3])
+print(a)
+
+#Q5
+d={'name':'lovisha',
+   'age':20}
+print(d)
+
+a=input("enter your name")
+b=input("enter your age")
+d={'name':a,
+   'age':b}
+print(d)
+
+#Q7
+a=("MISSISSIPPI")
+print(a.count('S'))
+
